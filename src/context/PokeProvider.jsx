32cd580da4ffb7faac0e482dc47baed577c8151d
Claudia Villarroel <claudia.villarroel@live.com>
2023-11-05 20:31:12 -0300

    :sparkles: Detalle pokemon

--- src/context/PokeProvider.jsx
+++ src/context/PokeProvider.jsx
@@ -6,16 +6,24 @@ import axios from "axios";
 const URLBase = "https://pokeapi.co/api/v2/pokemon/";
 export const PokeProvider = ({ children }) => {
   const [pokemones, setPokemones] = useState([]);
+  const [id, setId] = useState(0);
+  const [pokemon, setPokemon] = useState({});
+
   useEffect(() => {
     axios.get(URLBase).then((res) => {
-     // console.log(res.data.results);
       setPokemones(res.data.results);
-      console.log(pokemones);
     });
   }, []);
 
+  useEffect(() => {
+    if (id != 0) {
+      axios.get(URLBase + id).then((res) => {
+        setPokemon(res.data);
+      });
+    }
+  }, [id]);
   return (
-    <PokeContext.Provider value={{ pokemones }}>
+    <PokeContext.Provider value={{ pokemones, setPokemon, pokemon, setId }}>
       {children}
     </PokeContext.Provider>
   );
--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
-
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { PokeContext } from "../../context/PokeContext";
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+  const { pokemon, setId } = useContext(PokeContext);
+  const { id } = useParams();
 
-export default Details
+  useEffect(() => {
+    setId(id);
+  }, [id]);
+
+  return (
+    <div>
+      {pokemon ? (
+        <>
+          <div>
+            <h1 className="text-3xl font-bold uppercase text-center mt-5">
+              {pokemon.name}
+            </h1>
+            <div className="flex justify-center items-center">
+              <h3 className="text-center text-xl mt-2 font-bold"> Tipo(s) : </h3>
+              {pokemon.types ?
+                (pokemon.types.map((type) => (
+                  <h3 className="text-center text-xl mt-2 uppercase" key={type.type.name}>
+                    {type.type.name}
+                  </h3>
+                ))) : (<h3  className="text-center text-xl mt-2 ">Desconocido</h3>)}
+            </div>
+
+            <div className="flex justify-center items-center">
+              <div>
+                {pokemon.sprites ? (
+                  <img src={pokemon.sprites.front_default} alt="" width={300} />
+                ) : null}
+              </div>
+              <div className="flex flex-col items-start content-center">
+                {pokemon.stats &&
+                  pokemon.stats.map((stat) => (
+                    <div className="flex justify-center" key={stat.stat.name}>
+                      <label className="font-bold">{stat.stat.name} : </label>
+                      <p>{stat.base_stat}</p>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </div>
+  );
+};
+
+export default Details;
