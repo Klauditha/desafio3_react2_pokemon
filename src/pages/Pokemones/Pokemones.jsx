1336a08e816faa63f633a535a9223c66bb1b4762
Claudia Villarroel <claudia.villarroel@live.com>
2023-11-05 19:07:06 -0300

    :sparkles: Boton con accion ir a detalle

--- src/pages/Pokemones/Pokemones.jsx
+++ src/pages/Pokemones/Pokemones.jsx
@@ -1,8 +1,11 @@
 import { useContext } from "react";
 import { PokeContext } from "../../context/PokeContext";
+import { useNavigate } from "react-router-dom";
 
 const Pokemones = () => {
   const { pokemones } = useContext(PokeContext);
+  const navigate = useNavigate();
+
   return (
     <div className="home font-serif text-center">
       <h1 className="text-3xl font-bold p-5">Selecciona un Pokemón</h1>
@@ -14,15 +17,27 @@ const Pokemones = () => {
         >
           {pokemones.map((pokemon) => (
             <option
-              key={pokemon.name}
+              key={pokemon.url.split("/")[6]}
               className="m-6"
-              value={pokemon.url.split("/")[6]}
+              value={pokemon.url}
             >
               {pokemon.name.toUpperCase()}
             </option>
           ))}
         </select>
       </div>
+      <button
+        className="mt-5 bg-black hover:bg-black text-white font-bold py-2 px-4 rounded text-sm"
+        onClick={() =>
+          navigate(
+            `/pokemon/${
+              document.getElementById("pokemones").value.split("/")[6]
+            }`
+          )
+        }
+      >
+        Ver Detalle
+      </button>
     </div>
   );
 };
