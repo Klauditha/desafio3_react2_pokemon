63b3ba86f9dadc9c79f58ace1384931344a83024
Claudia Villarroel <claudia.villarroel@live.com>
2023-11-05 09:14:11 -0300

    :sparkles: Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import imagen from "../../assets/img/profesorOak.png";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home font-serif text-center">
+      <h1 className="text-3xl font-bold p-5">Bienvenido Maestro Pokemón</h1>
+      <div className="flex justify-center">
+        <img src={imagen} alt="profesor Oak" />
+      </div>
+      <p className="p-5">
+        En este sitio encontrarás toda la información necesario para aprender
+        sobre los pokemónes y ayudarte a enfrentar cada batalla.
+      </p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
